b5c112e0cabeac5f56e55a86838cbdabc1b41ca7
Tony CABAYE <tony.cabaye@smile.fr>
2024-10-01 16:08:05 +0200

    feat: show incoming garbage

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -4,6 +4,7 @@ import { GameState } from "../types/logic.ts"
 import NextBlock from "./NextBlock.tsx"
 import Well from "./Well.tsx"
 import Controls from "./Controls.tsx"
+import { createArray } from "@tonai/game-utils"
 
 export interface IGameProps {
   game: GameState
@@ -12,12 +13,23 @@ export interface IGameProps {
 
 export default function Game(props: IGameProps) {
   const { game, playerId } = props
-  const playerState = game.playersState[playerId]
+  const { playersState, playersGarbage } = game
+  const playerState = playersState[playerId]
+  const playerGarbage = playersGarbage.find(({ id }) => id === playerId)
+  console.log(playerGarbage)
+  const totalGarbage = createArray(
+    playerGarbage?.rows.reduce((a, b) => a + b, 0) ?? 0
+  )
   return (
     <div className="game">
       <Well playerState={playerState} />
       <div className="game__column">
         <NextBlock block={playerState.sequence[0]} />
+        <div className="game__garbages">
+          {totalGarbage.map((_, i) => (
+            <div key={i} className="game__garbage"></div>
+          ))}
+        </div>
       </div>
       {!game.playersState[playerId].gameOver && <Controls />}
     </div>
